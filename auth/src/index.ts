d9222bdab87b24f273747591287f1d1432cb46c3
Rachidoulgour <olgor@hotmail.com>
2024-05-16 14:06:48 +0200

    skaffold and ingress server added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi Amigos!');
+});
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
